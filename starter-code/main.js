3b985740447f17edee9cff6c924b839ba1f3f16d
Jaime Gunther <jaimegunther@Jaimes-MacBook-Air.local>
2016-09-22 14:30:34 +1000

    created comparison code

--- starter-code/main.js
+++ starter-code/main.js
@@ -3,3 +3,17 @@ var cardOne = "queen";
 var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
+
+if (cardTwo !== cardFour || cardTwo !== cardThree) {
+	alert('Sorry, try again.') 
+} else {
+	alert('You found a match!')
+};
+
+if (cardOne !== cardFour || cardOne !== cardThree) {
+	alert('Sorry, try again.') 
+} else {
+	alert('You found a match!')
+};
+
+
