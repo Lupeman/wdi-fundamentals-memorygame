0c13e01e6269d91547156030f4f4c83baa770f38
Jaime Gunther <jaimegunther@Jaimes-MacBook-Air.local>
2016-11-08 13:09:25 +1100

    updated code and added images

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,33 +1,88 @@
-console.log("JS file is connected to HTML! Woo!")
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
-/*
-if (cardTwo !== cardFour || cardTwo !== cardThree) {
-	alert('Sorry, try again.') 
-} else {
-	alert('You found a match!')
-};
-
-if (cardOne !== cardFour || cardOne !== cardThree) {
-	alert('Sorry, try again.') 
-} else {
-	alert('You found a match!')
-};
-*/
-
+// Create the playing board
 var board = document.getElementById('game-board');
-var createCards = function() {
 
-for (var i = 0; i < 4; i++) {
-	var cardList = document.createElement('div');
-	cardList.className = 'card';
-	board.appendChild(cardList);
+	
+// Create the cards
+var cards = ['queen', 'queen', 'king', 'king'];
+var cardsInPlay = [];
+
+var createBoard = function() {
+for (var i = 0; i < cards.length; i++) {
+	var cardElement = document.createElement('div');
+	cardElement.className = 'card';
+	cardElement.setAttribute('data-card', cards[i]);
+	cardElement.addEventListener('click', isTwoCards);
+	board.appendChild(cardElement);
+	}
+};
+
+/* 
+When a card is clicked show the image and then check to 
+see if two cards have been selected
+*/
+var isTwoCards = function(){
+	cardsInPlay.push(this.getAttribute('data-card'));
+	if (this.getAttribute('data-card') === 'king') {
+	this.innerHTML = '<img src="Seuss_King.jpeg" class = "myImgClass" alt="King" />';	
+} else {
+	this.innerHTML = '<img src="Seuss_Queen.png" class = "myImgClass" alt="Queen" />';
+}
+	
+	if (cardsInPlay.length === 2) {
+		setTimeout(function() {
+			isMatch(cardsInPlay);
+		}, 300);
 }
 };
 
-createCards();
+/* 
+Check to see if the cards match. 
+If they don't, alert no match and reset the 
+cards back. Otherwise alert a match
+*/
+var isMatch = function() {
+if (cardsInPlay[0] !== cardsInPlay[1]) {
+	alert('Sorry, try again.');
+	resetGame();
+} else {
+	alert('You found a match!')
+};
+cardsInPlay = [];
+
+}
+
+createBoard();
+
+// Function to reset the game ie.: flip cards back
+var resetGame = function() {
+	cardsInPlay = [];
+	var tryIt = document.getElementsByClassName('card');
+	for (var x = 0; 0 <tryIt.length; x++) {
+	tryIt[x].innerHTML = "";
+	}
+};
+
+// Create a reset button
+var button = document.getElementById('btn');
+
+// Allow for button to reset game once clicked
+button.addEventListener('click', resetGame);
+
+// Function to show information about game creator
+var alertUser = function () {
+    alert("Hey my name is Jaime and I'm the creator of this awesome game! I'm currently studying to become a Web Developer at GA. Looking forward to creating some even more awesome content!");
+}
+
+// When link is clicked run alertUser function to show info about creator
+var link = document.getElementById('aboutMe');
+
+link.addEventListener('click', alertUser);
+
+
+
+
+
+
 
 
 
